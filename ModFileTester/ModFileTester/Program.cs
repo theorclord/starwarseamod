68b93ee9a16cab45dbc1d4c784d74479129b87f6
theorclord <mstolbor@gmail.com>
2021-10-24 21:39:02 +0200

    First commit, the files contains normal amount of units. The text file have been modified

---
+++ ModFileTester/ModFileTester/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ModFileTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            string myFilePath = @"C:\projects\starwarseamod\My_Mod\Data\XML\CAMPAIGNS_UNDERWORLD_GC.XML";
+            string compareFilePath = @"C:\projects\starwarseamod\CompareFiles\CAMPAIGNS_UNDERWORLD_GC.XML";
+
+            string myFileContent = "";
+            using(TextReader reader = new StreamReader(myFilePath))
+            {
+                myFileContent = reader.ReadToEnd();
+            }
+
+            string compareContent = "";
+            using (TextReader reader = new StreamReader(compareFilePath))
+            {
+                compareContent = reader.ReadToEnd();
+            }
+
+            string[] myFileLines = myFileContent.Split("\r\n");
+            string[] compareFileLines = compareContent.Split("\r\n");
+
+
+            Dictionary<int, string> lineDifference = new Dictionary<int, string>();
+            for(int i = 0; i< myFileLines.Length; i++)
+            {
+
+                if(myFileLines[i] != compareFileLines[i])
+                {
+                    lineDifference.Add(i, compareFileLines[i]);
+                }
+
+            }
+
+            foreach(KeyValuePair<int,string> lineDiff in lineDifference)
+            {
+                Console.WriteLine($"{lineDiff.Key}: {lineDiff.Value}");
+            }
+
+        }
+    }
+}
